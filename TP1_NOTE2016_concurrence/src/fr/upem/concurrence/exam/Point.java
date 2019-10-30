d6ebb6bb3d311ec93c9c366b63bb73a2e9e39b9f
JonathanCrt <jonathan.crete@outlook.fr>
2019-10-30 18:10:52 +0100

    tp note concurrence 2018

--- TP1_NOTE2016_concurrence/src/fr/upem/concurrence/exam/Point.java
+++ TP1_NOTE2016_concurrence/src/fr/upem/concurrence/exam/Point.java
@@ -1,79 +1,87 @@
 package fr.upem.concurrence.exam;
 
 public class Point {
-    private int x;
-    private int y;
-    private final Object lock = new Object();
+	private int x;
+	private int y;
+	private final Object lock = new Object();
 
-    public Point(int x, int y) {
-        synchronized (lock) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+	public Point(int x, int y) {
+		synchronized (lock) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 
-    public boolean isOrigin() {
-        return x == 0 && y == 0;
-    }
+	public boolean isOrigin() {
+		return x == 0 && y == 0;
+	}
 
-    public void addX(int dx) {
-        synchronized (lock) {
-            x = x + dx;
-        }
-    }
+	public void addX(int dx) {
+		synchronized (lock) {
+			x = x + dx;
+		}
+	}
 
-    public void addY(int dy) {
-        synchronized (lock) {
-            y = y + dy;
-        }
-    }
+	public void addY(int dy) {
+		synchronized (lock) {
+			y = y + dy;
+		}
+	}
 
-    public int getX() {
-        synchronized (lock) {
-            return x;
-        }
-    }
+	public void addXY(int dx, int dy) {
+		synchronized (lock) {
+			x = x + dx;
+			y = y + dy;
+		}
+	}
 
-    public int getY() {
-        synchronized (lock) {
-            return y;
-        }
-    }
+	/*
+	 * public int getX() { synchronized (lock) { return x; } }
+	 * 
+	 * public int getY() { synchronized (lock) { return y; } }
+	 */
 
-    @Override
-    public String toString() {
-        synchronized (lock) {
-            return "(" + x + "," + y + ")";
-        }
-    }
+	@Override
+	public String toString() {
+		synchronized (lock) {
+			return "(" + x + "," + y + ")";
+		}
+	}
 
-    public static void main(String[] args) {
-        Point p = new Point(0, 0);
-        new Thread(() -> {
-            for (;;) {
-                synchronized (p.lock) {
-                    p.addX(1);
-                    p.addY(1);
-                }
-            }
-        }).start();
+	public void display() {
+		synchronized (lock) {
+			if (this.x != this.y) {
+				System.out.println("C'est étrange, non?");
+			}
+		}
+	}
 
-        new Thread(() -> {
-            for (;;) {
-                synchronized (p.lock) {
-                    p.addX(-1);
-                    p.addY(-1);
-                }
-            }
-        }).start();
+	public static void main(String[] args) {
+		Point p = new Point(0, 0);
+		var lmainLock = new Object();
+		new Thread(() -> {
+			for (;;) {
+				p.addXY(1, 1);
+			}
+		}).start();
 
-        for (;;) {
-            if (p.getX() != p.getY()) {
-                System.out.println("C'est étrange, non?");
-            }
-            if (p.isOrigin()) {
-                System.out.println("Retour à la case départ : " + p);
-            }
-        }
-    }
+		new Thread(() -> {
+			for (;;) {
+					p.addXY(-1, -1);
+			}
+		}).start();
+
+		for (;;) {
+			p.display();
+			p.displayOrigin();
+		}
+	}
+
+	private void displayOrigin() {
+		synchronized (lock) {
+			if (x == 0 && y == 0) {
+				System.out.println("Retour à la case départ : " + this);
+			}
+		}
+	}
 }
